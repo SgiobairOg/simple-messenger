a8ac83b62102ea5d9eb6edd91dd000bed0653720
Jason Wilson <jason@wilsons.io>
2016-12-12 13:31:29 -0500

    Subject Line
    Why:
    
    * Continuing initial development
    
    Changes made to address the need:
    
    * Adds Silex route for testing
    * Adds consumer logic for CloudAMQP

--- web/index.php
+++ web/index.php
@@ -5,6 +5,16 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+//Configure details of the RabbitMQ service
+define('AMQP_DEBUG', true);
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+$url = parse_url(getenv('CLOUDAMQP_URL'));
+
+//Symphony
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
@@ -18,8 +28,82 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 // Messenger handlers
 $messenger = $app['controllers_factory'];
 $messenger->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output from messenger controller.');
-  return $app['twig']->render('index.twig');
+  $app['monolog']->addDebug('fetching message.');
+  
+  //Connect to the queue
+  $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
+  $channel = $conn->channel();
+
+  $exchange = 'amq.direct';
+  $queue = 'basic_get_queue';
+  $channel->queue_declare($queue, false, true, false, false);
+  $channel->exchange_declare($exchange, 'direct', true, true, false);
+  $channel->queue_bind($queue, $exchange);
+  
+  $app['monolog']->addDebug('Waiting for messages');
+  
+  $deliver = function($msg) {
+    echo ' [X] ', $msg->body, '\n';
+  };
+  
+  //Retrive message from queue
+  $channel->basic_consume($queue, '', false, true, false, false, $deliver);
+  
+  while(count($channel->callbacks)) {
+    $channel->wait();
+  }
+  
+  //Disconnect from the queue
+  $channel->close();
+  $conn->close();
+});
+
+$messenger->get('/send/test/{message}', function($message) use($app) {
+  $app['monolog']->addDebug('sending message.');
+  
+  //Connect to the queue
+  $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
+  $channel = $conn->channel();
+
+  $exchange = 'amq.direct';
+  $queue = 'basic_get_queue';
+  $channel->queue_declare($queue, false, true, false, false);
+  $channel->exchange_declare($exchange, 'direct', true, true, false);
+  $channel->queue_bind($queue, $exchange);
+  
+  //Send message to the queue
+  $msg_body = $message;
+  $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
+  $channel->basic_publish($msg, $exchange);
+  
+  //Disconnect from the queue
+  $channel->close();
+  $conn->close();
+});
+
+$messenger->post('/send', function(Request $request) use($app) {
+  $app['monolog']->addDebug('sending message.');
+  
+  //Connect to the queue
+  $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
+  $channel = $conn->channel();
+
+  $exchange = 'amq.direct';
+  $queue = 'basic_get_queue';
+  $channel->queue_declare($queue, false, true, false, false);
+  $channel->exchange_declare($exchange, 'direct', true, true, false);
+  $channel->queue_bind($queue, $exchange);
+  
+  //Send message to the queue
+  $msg_body = $request->get('message');
+  $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
+  $channel->basic_publish($msg, $exchange);
+  
+  //Disconnect from the queue
+  $channel->close();
+  $conn->close();
+  
+  return new Response('Message sent!', 201);
 });
 
 $app->mount('/messenger', $messenger);
