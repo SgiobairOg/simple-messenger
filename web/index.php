0070578cab8e9ccac54a71f12fc9f7f0c9983746
Jason Wilson <jason@wilsons.io>
2016-12-12 14:47:57 -0500

    debugging last commit

--- web/index.php
+++ web/index.php
@@ -5,6 +5,11 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+//Configure details of the RabbitMQ service
+define('AMQP_DEBUG', true);
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+$url = parse_url(getenv('CLOUDAMQP_URL'));
 
 //Symphony
 use Symfony\Component\HttpFoundation\Request;
@@ -22,112 +27,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 // Messenger handlers
 $messenger = $app['controllers_factory'];
-
-//Test Emitter
 $messenger->get('/emit/test', function() use($app) {
-  
-  // Log
-  $app['monolog']->addDebug('Sending message.');
-  
-  //Configure details of the RabbitMQ service
-  define('AMQP_DEBUG', true);
-  use PhpAmqpLib\Connection\AMQPConnection;
-  use PhpAmqpLib\Message\AMQPMessage;
-  
-  //Get cloudAMQP url from server
-  $url = parse_url(getenv('CLOUDAMQP_URL'));
-  
-  
-  //Connect to the queue
-  $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
-  $channel = $conn->channel();
-
-  //Declare exchange
-  $exchange = 'amq.fanout';
-  $channel->exchange_declare($exchange, 'fanout', false, false, false);
-  
-  $data = implode( ' ', array_slice($argv,1));
-  if(empty($data)) $data = "info: Hello World!";
-  $msg = new AMQPMessage($data);
-  
-  $channel->basic_publish($msg, $exchange);
-  
-  echo " [X] Sent: ", $data, "\n";
-  
-  //Disconnect from the queue
-  $channel->close();
-  $conn->close();
-  
-  return new Response('Message Sent!', 201);
-});
-
-//Test consumer
-$messenger->get('/consume/test', function() use($app) {
   $app['monolog']->addDebug('fetching message.');
   
-  //Configure details of the RabbitMQ service
-  define('AMQP_DEBUG', true);
-  use PhpAmqpLib\Connection\AMQPConnection;
-  use PhpAmqpLib\Message\AMQPMessage;
+  echo 'Emit test';
   
-  //Get cloudAMQP url from server
-  $url = parse_url(getenv('CLOUDAMQP_URL'));
-  
-  
-  //Connect to the queue
-  $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
-  $channel = $conn->channel();
-  
-  //Declare exchange
-  $exchange = 'amq.fanout';
-  $channel->exchange_declare($exchange, 'fanout', false, false, false);
-  
-  list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
-  
-  $channel->queue_bind($queue_name, $exchange);
-  
-  echo '  [X] Waiting for logs.', '\n';
-  
-  $callback = function( $message ) {
-    echo '    [X] ', $message->body, '\n';
-  };
-  
-  $channel->basic_consume($queue_name, '', false, true, false, false, $callback);
-  
-  while(count($channel->callbacks)) {
-    $channel->wait();
-  }
-  
-  $channel->close();
-  $connection->close();
-  
-  return new Response('Listening...', 201);
+  return;
 });
 
-$messenger->post('/send', function(Request $request) use($app) {
-  $url = parse_url(getenv('CLOUDAMQP_URL'));
+$messenger->get('/consume/test', function($message) use($app) {
   $app['monolog']->addDebug('sending message.');
   
-  //Connect to the queue
-  $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
-  $channel = $conn->channel();
-
-  $exchange = 'amq.direct';
-  $queue = 'basic_get_queue';
-  $channel->queue_declare($queue, false, true, false, false);
-  $channel->exchange_declare($exchange, 'direct', true, true, false);
-  $channel->queue_bind($queue, $exchange);
+  echo 'Consume test';
   
-  //Send message to the queue
-  $msg_body = $request->get('message');
-  $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
-  $channel->basic_publish($msg, $exchange);
-  
-  //Disconnect from the queue
-  $channel->close();
-  $conn->close();
-  
-  return new Response('Message sent!', 201);
+  return;
 });
 
 $app->mount('/messenger', $messenger);
